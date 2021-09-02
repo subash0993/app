3b138dc7bc7a37de69b5a5cbe764fbc4ec0bee6a
Subash <53207783+subash0993@users.noreply.github.com>
2021-09-02 12:56:15 +0530

    add file webapp url
    url file for webapp added

--- urls.py
+++ urls.py
@@ -1,11 +1,22 @@
-from django.urls import path
+"""webapp URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('display', views.display_students, name='displaystudents'),
-    path('add', views.add_students, name='addstudent'),
-    path('find', views.get_student, name='getstudent'),
-    path('delete', views.delete_student, name='deletestudent'),
+    path('', include('students.urls')),
+    path('admin/', admin.site.urls),
 ]
